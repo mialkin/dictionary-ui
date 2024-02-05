f81ff8eab297f665faf07e183c5db119c71db11d
Aleksei Mialkin <aleksei@mialkin.ru>
2024-02-05 15:14:14 +0300

    Update default files

---
+++ src/app/words/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default function Home() {
+    return (
+        <main>
+            <div className={styles.word}>
+                This is words page :-)
+            </div>
+        </main>
+    );
+}
