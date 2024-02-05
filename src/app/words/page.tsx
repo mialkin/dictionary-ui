7efb8c0ea1f08cd856c26e66614b667a33638976
Aleksei Mialkin <aleksei@mialkin.ru>
2024-02-05 15:26:55 +0300

    Use effect

--- src/app/words/page.tsx
+++ src/app/words/page.tsx
@@ -1,10 +1,28 @@
-import styles from './page.module.css'
+'use client'
 
-export default function Home() {
+import styles from './page.module.css'
+import Link from "next/link";
+import React, { useEffect } from "react";
+
+export default function Words() {
+
+    useEffect(() => {
+        const keyDownHandler = (event: { code: any; }) => console.log(`You pressed ${event.code}.`);
+        document.addEventListener("keydown", keyDownHandler);
+
+        return () => {
+            console.log('Removing event listener')
+            document.removeEventListener("keydown", keyDownHandler);
+        };
+    }, []);
+
     return (
         <main>
             <div className={styles.word}>
                 This is words page :-)
+            </div>
+            <div>
+                <Link href="/">На главную</Link>
             </div>
         </main>
     );
