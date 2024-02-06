8c0b074ce860f3813be8a5472ef1deb55975b6be
Aleksei Mialkin <aleksei@mialkin.ru>
2024-02-06 21:36:16 +0300

    Remove 'use client'

--- src/app/(words)/words/page.tsx
+++ src/app/(words)/words/page.tsx
@@ -1,13 +1,10 @@
-'use client'
-
-import React, { useEffect } from "react";
 import WordsList from "@/app/(words)/words/components/words-list";
 import WordsSearchbar from "@/app/(words)/words/components/words-searchbar";
 
 export default function Words({ searchParams, }: { searchParams?: { term?: string; language?: number; }; }) {
 
-    const term = searchParams?.term || '';
     let languageId = searchParams?.language || 1;
+    const term = searchParams?.term || '';
 
     return (
         <div>
