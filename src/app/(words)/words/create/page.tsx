93d342800474360beb1d05a9584e6d5c0d6e19ab
Aleksei Mialkin <aleksei@mialkin.ru>
2024-02-22 07:58:51 +0300

    Implement word creation

--- src/app/(words)/words/create/page.tsx
+++ src/app/(words)/words/create/page.tsx
@@ -2,15 +2,19 @@
 
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function CreateWord() {
     const searchParams = useSearchParams();
     const router = useRouter();
 
+    const [languageId, setLanguage] = useState(searchParams.get('language')?.toString());
+    const [name, setName] = useState(searchParams.get('q')?.toString());
+    const [transcription, setTranscription] = useState('');
+    const [translation, setTranslation] = useState('');
+
     useEffect(() => {
         const keyDownHandler = (event: any) => {
-
             if (event.code == 'Escape') {
                 router.push('/words')
             }
@@ -22,12 +26,18 @@ export default function CreateWord() {
             document.removeEventListener("keydown", keyDownHandler);
         };
     }, []);
+
     return (
         <main>
             <div>
                 <select
-                    defaultValue={searchParams.get('language')?.toString()}
-                    disabled={true}
+                    defaultValue={languageId}
+                    onChange={
+                        event => {
+                            let target = event.target as HTMLSelectElement;
+                            setLanguage(target.value);
+                        }
+                    }
                 >
                     <option value='1'>Английский</option>
                     <option value='2'>Французский</option>
@@ -37,18 +47,75 @@ export default function CreateWord() {
                 </select>
             </div>
             <div>
+                <label>Слово:</label>
                 <input
                     type='text'
                     name='name'
-                    defaultValue={searchParams.get('q')?.toString()}
+                    defaultValue={name}
+                    onInput={event => {
+                        let target = event.target as HTMLInputElement;
+                        setName(target.value);
+                    }}
                 />
             </div>
             <div>
-                <input />
+                <label>Транскрипция:</label>
+                <input type='text'
+                       name='transcription'
+                       onInput={event => {
+                           let target = event.target as HTMLInputElement;
+                           setTranscription(target.value);
+                       }}
+                />
+            </div>
+            <div>
+                <label>Перевод:</label>
+                <textarea name='translation'
+                          cols={70}
+                          rows={10}
+                          onInput={event => {
+                              let target = event.target as HTMLInputElement;
+                              setTranslation(target.value);
+                          }}
+                />
+            </div>
+            <div>
+                <button
+                    onClick={async () => {
+                        let success = await createWord(languageId!, name!, transcription, translation)
+                        if (success) {
+                            router.push('/words')
+                        }
+                    }}>
+                    Сохранить
+                </button>
             </div>
             <div>
                 <Link href="/words">К словам</Link>
             </div>
         </main>
     );
+}
+
+async function createWord(languageId: string, name: string, transcription: string, translation: string) {
+
+    const rawFormData = {
+        languageId: languageId,
+        name: name,
+        transcription: transcription,
+        translation: translation
+    }
+
+    let url = new URL('api/words/create', process.env.NEXT_PUBLIC_CLIENT_GATEWAY_API_URL)
+
+    const response = await fetch(url.toString(), {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(rawFormData),
+    });
+
+    return response.status == 200;
 }
