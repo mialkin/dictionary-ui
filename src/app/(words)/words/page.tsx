858ac5dee8e84ee3b2fe68b047045b31fc3e32f2
Aleksei Mialkin <aleksei@mialkin.ru>
2024-03-02 18:19:18 +0300

    Remove comment

--- src/app/(words)/words/page.tsx
+++ src/app/(words)/words/page.tsx
@@ -4,11 +4,10 @@ import WordsDictionarySelector from '@/app/(words)/words/components/words-dictio
 
 export default function Words() {
     return (
-            <div>
-                <WordsDictionarySelector />
-                <WordsSearchbar />
-                <WordsList />
-            </div>
-        // TODO Use public folder in the root where next.svg and vercel.svg used to be
+        <div>
+            <WordsDictionarySelector />
+            <WordsSearchbar />
+            <WordsList />
+        </div>
     );
 }
