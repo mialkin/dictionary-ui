62912291ee2fc431f06c213129f906269a59b0fb
Aleksei Mialkin <aleksei@mialkin.ru>
2024-03-05 23:51:26 +0300

    Update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from './page.module.css';
 
 export default function Home() {
     return (
-        <div>
-            <div>
-                <Link href={new URL('/login', process.env.NEXT_PUBLIC_CLIENT_GATEWAY_API_URL)}>Войти</Link>
-            </div>
+        <div className={styles.container}>
+            <header>
+                <div className={styles.login}>
+                    <Link href={new URL('/login', process.env.NEXT_PUBLIC_CLIENT_GATEWAY_API_URL)}>Войти</Link>
+                </div>
+            </header>
         </div>
     );
 }
