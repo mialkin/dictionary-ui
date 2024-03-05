17a6965aef20f190c1501cba6d7dea6b4dd1465d
Aleksei Mialkin <aleksei@mialkin.ru>
2024-03-06 01:04:08 +0300

    Improve layout

--- src/app/(words)/words/layout.tsx
+++ src/app/(words)/words/layout.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import Link from 'next/link';
 import styles from './layout.module.css';
 
 export default function WordsLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div>
-            <div className={styles.header}>
+        <div className={styles.container}>
+            <header>
                 <form action={new URL('/logout', process.env.NEXT_PUBLIC_CLIENT_GATEWAY_API_URL).toString()}
                       method='POST'>
                     <input type='submit' value='Выйти' />
                 </form>
-            </div>
-            <div className={styles.main}>
+            </header>
+            <div>
                 {children}
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,7 @@ import styles from './page.module.css';
 export default function Home() {
     return (
         <div className={styles.container}>
-            <header>
-                <div className={styles.login}>
-                    <Link href={new URL('/login', process.env.NEXT_PUBLIC_CLIENT_GATEWAY_API_URL)}>Войти</Link>
-                </div>
-            </header>
+            <Link href={new URL('/login', process.env.NEXT_PUBLIC_CLIENT_GATEWAY_API_URL)}>Войти</Link>
         </div>
     );
 }
